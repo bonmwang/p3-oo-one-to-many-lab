bdf91097d9d7177e77785a3d19095ebb28132f14
Boniface <bm715452@gmail.com>
2025-05-17 17:27:12 +0300

    oo one to many labs

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,34 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception(f"Invalid pet type. Must be one of {self.PET_TYPES}")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        self.__class__.all.append(self)
+
+    def __repr__(self):
+        return f"<pet name='{self.name}' type='{self.pet_type}'>"
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("The pet must be an instance of pet class.")
+        
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+    
+    def __repr__(self):
+        return f"<Owner name='{self.name}'>"
